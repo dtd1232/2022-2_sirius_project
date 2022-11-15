3059e8d1c3503ed36203ffcccb486efc10a9f814
HyeunKim <hyeeunkim1230@gmail.com>
2022-11-15 13:38:58 +0900

    update summoned animal part
    When J animal is summoned, J image(temporary img) is shown and player can't use skill J.

--- Sirius_project_1/Assets/Script/Hyeeun/AnimalStatus.cs
+++ Sirius_project_1/Assets/Script/Hyeeun/AnimalStatus.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimalStatus : MonoBehaviour
 {
     // 동물이 소환되었는지 여부
-    private bool J_animal_summonded;
-    private bool K_animal_summonded;
-    private bool L_animal_summonded;
+    public bool J_animal_summonded;
+    public bool K_animal_summonded;
+    public bool L_animal_summonded;
 
     // 스킬들 사용할 수 있는지 여부
-    private bool J_skill_can_use;
-    private bool K_skill_can_use;
-    private bool L_skill_can_use;
+    public bool J_skill_can_use;
+    public bool K_skill_can_use;
+    public bool L_skill_can_use;
+
+    public bool is_summonded;
+    public Image summondedAnimal;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +25,8 @@ public class AnimalStatus : MonoBehaviour
         J_animal_summonded = true;
         K_animal_summonded = false;
         K_animal_summonded = false;
+
+        is_summonded = J_animal_summonded || K_animal_summonded || L_animal_summonded;
 
         // 스킬은 동물이 소환되면 사용할 수 없음.
         J_skill_can_use = !J_animal_summonded;
